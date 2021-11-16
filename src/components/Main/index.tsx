168fbbd7ed1fc8fee7a3e5b94be4ab0888be8a4b
Italo Felipe <felipe.italo@hotmail.com>
2021-11-16 08:47:08 -0300

    chore: changed some settings and added react-leaflet

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,22 +1,9 @@
-import * as Styled from "./styles";
-
-const Main = ({
-	title = "React Avançado",
-	description = "TypeScript, React.js, Next.js e Styled Components"
-}) => {
+const Main = () => {
 	return (
-		<Styled.Wrapper>
-			<Styled.Logo
-				src="/img/logo.svg"
-				alt="Imagem de um atomo e React Avançado escrito ao lado."
-			/>
-			<Styled.Title>{title}</Styled.Title>
-			<Styled.Description>{description}</Styled.Description>
-			<Styled.Illustration
-				src="/img/hero-illustration.svg"
-				alt="Um desenvolvedor de frente para tela com codigo"
-			/>
-		</Styled.Wrapper>
+		<div>
+			<h1>NextJS na prática</h1>
+			<a href="#">Link</a>
+		</div>
 	);
 };
 
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,16 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+
+  :root {
+    --highlight: #e20e8d;
+    --background: #030518;
+    --white: #eeeeee;
+    --container: 100rem;
+    --small: 1.5rem;
+    --medium: 3rem;
+    --large: 5rem;
+  }
   * {
     margin: 0;
     padding: 0;
@@ -13,7 +23,18 @@ const GlobalStyles = createGlobalStyle`
     height: 100%;
   }
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background: var(--background);
+    color: var(--white);
+  }
+
+  p, a {
+    font-size: 2rem;
+    line-height: var(--medium);
+    
+  }
+  a {
+    color: var(--highlight);
   }
 `;
 
